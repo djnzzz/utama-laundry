27f15ade41da03432f0fd95dae2b87f1dd2940b4
djnzzz <derbyjunio@gmail.com>
2025-12-12 15:52:24 +0700

    penambahan icon

--- resources/views/admin/order-detail.blade.php
+++ resources/views/admin/order-detail.blade.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Detail Pesanan - Admin Utama Laundry</title>
+
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicons/favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ asset('favicons/site.webmanifest') }}">
+
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
     <link rel="stylesheet" href="{{ asset('css/admin-orders.css') }}">
--- resources/views/pages/editprofil.blade.php
+++ resources/views/pages/editprofil.blade.php
@@ -5,11 +5,17 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>Profil Akun - Utama Laundry</title>
 
+<link rel="icon" type="image/x-icon" href="{{ asset('favicons/favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ asset('favicons/site.webmanifest') }}">
+
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet" />
 <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
 
 <style>
-#profileImageInput { display: none; } /* Hilangkan choose file */
+#profileImageInput { display: none; }
 </style>
 
 </head>
